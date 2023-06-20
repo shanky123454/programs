616aa54ade95ea921cb675219805923ebc169a7d
shashank <shashank.raj@gmail.com>
2023-06-21 03:08:55 +0530

    hurray

--- checking.java
+++ checking.java
@@ -7,6 +7,6 @@ class Paddu{
         //System.out.println(p instanceof Thread);//C.E
         System.out.println(p instanceof Object);
         System.out.println(p instanceof Paddu);
-        System.out.println(Class.forName(args[0]).isInstance(p));
+        System.out.println(Class.forName(args[0]).isInstance(p));   
     }
 }
